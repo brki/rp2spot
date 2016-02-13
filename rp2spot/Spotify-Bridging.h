0a5eeccb11f4570e6042d63834e24b3ff0553080
Brian King <free.as.in.speech@gmail.com>
2016-02-13 11:13:13 +0100

    Error-free build with AlamoFire and Spotify frameworks

---
+++ rp2spot/Spotify-Bridging.h
@@ -0,0 +1,14 @@
+//
+//  Spotify-Bridging.h
+//  rp2spot
+//
+//  Created by Brian on 13/02/16.
+//  Copyright © 2016 truckin'. All rights reserved.
+//
+
+#ifndef Spotify_Bridging_h
+#define Spotify_Bridging_h
+
+#import <Spotify/Spotify.h>
+
+#endif /* Spotify_Bridging_h */
